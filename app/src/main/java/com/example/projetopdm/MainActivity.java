3b859196c97ed38a97cf1271854eb0c3dd9acbe9
xirucas <framciscoalbernaz.16@gmail.com>
2024-01-03 14:12:17 +0000

    ss

--- app/src/main/java/com/example/projetopdm/MainActivity.java
+++ app/src/main/java/com/example/projetopdm/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.projetopdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.projetopdm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); // Set the root view of the binding object
+        
+        setContentView(binding.getRoot());
     }
 }
